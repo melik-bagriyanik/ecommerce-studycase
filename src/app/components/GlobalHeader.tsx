07b3f99de231a1237d712788a89301872451a202
melik-bagriyanik <melikbagriyanik90@gmail.com>
2025-08-10 00:14:26 +0300

    fixed bg color bug

--- src/app/components/GlobalHeader.tsx
+++ src/app/components/GlobalHeader.tsx
@@ -63,7 +63,7 @@ export default function GlobalHeader() {
   };
 
   return (
-    <nav className="flex items-center justify-between px-4 py-3 md:p-6 max-w-7xl mx-auto">
+    <nav style={{ padding: '20px 80px' }} className="flex bg-sky-50 items-center justify-between px-4 py-3 md:p-6  mx-auto">
       <div className="flex items-center space-x-2">
         <Link href="/" className="flex items-center space-x-2">
           <div className="w-8 h-8 bg-gradient-to-r from-blue-600 to-purple-600 rounded-lg" />
